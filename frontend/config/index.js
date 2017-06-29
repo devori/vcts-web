0fa1e83efed7efff327279da418911cf347e025b
devori.lee <devori.lee@gmail.com>
2017-06-30 02:05:02 +0900

    init data file. add login-logout page

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:3000/api',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
--- frontend/src/containers/Home.vue
+++ frontend/src/containers/Home.vue
@@ -1,122 +1,91 @@
 <template>
-  <v-app id="example-1">
+  <v-app>
     <v-navigation-drawer persistent v-model="drawer" light enable-resize-watcher>
       <v-list dense>
         <v-list-item>
           <v-list-tile @click.native.stop="left = !left">
             <v-list-tile-action>
-              <v-icon>exit_to_app</v-icon>
+              <v-icon>edit</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
+              <v-list-tile-title>로그인</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-navigation-drawer persistent v-model="drawerRight" light right light enable-resize-watcher>
-      <v-list dense>
-        <v-list-item>
-          <v-list-tile @click.native.stop="right = !right">
-            <v-list-tile-action>
-              <v-icon>exit_to_app</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list-item>
+        <v-divider dark></v-divider>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar class="indigo" light>
       <v-toolbar-side-icon light @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Toolbar</v-toolbar-title>
+      <v-toolbar-title>Virtual Currency Trading System</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-side-icon light @click.native.stop="drawerRight = !drawerRight"></v-toolbar-side-icon>
     </v-toolbar>
     <main>
-      <v-navigation-drawer
-        temporary
-        v-model="left"
-      ></v-navigation-drawer>
       <v-container fluid>
-        <!--v-router-->
+        <v-alert info v-bind:value="true">
+          {{ result }}
+        </v-alert>
+        <v-layout row>
+          <v-flex xs4>
+            <v-subheader>Username</v-subheader>
+          </v-flex>
+          <v-flex xs8>
+            <v-text-field v-model="username"></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs4>
+            <v-subheader>Password</v-subheader>
+          </v-flex>
+          <v-flex xs8>
+            <v-text-field v-model="password"></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-btn round primary light v-on:click.native="login">Login</v-btn>
+        <v-btn round primary light v-on:click.native="logout">Logout</v-btn>
       </v-container>
-      <v-navigation-drawer
-        right
-        temporary
-        v-model="right"
-      ></v-navigation-drawer>
     </main>
     <v-footer class="indigo">
       <span>© 2017</span>
     </v-footer>
   </v-app>
 </template>
-<template>
-  <v-app id="example-1">
-    <v-navigation-drawer persistent v-model="drawer" light enable-resize-watcher>
-      <v-list dense>
-        <v-list-item>
-          <v-list-tile @click.native.stop="left = !left">
-            <v-list-tile-action>
-              <v-icon>exit_to_app</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-navigation-drawer persistent v-model="drawerRight" light right light enable-resize-watcher>
-      <v-list dense>
-        <v-list-item>
-          <v-list-tile @click.native.stop="right = !right">
-            <v-list-tile-action>
-              <v-icon>exit_to_app</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar class="indigo" light>
-      <v-toolbar-side-icon light @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Toolbar</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-side-icon light @click.native.stop="drawerRight = !drawerRight"></v-toolbar-side-icon>
-    </v-toolbar>
-    <main>
-      <v-navigation-drawer
-        temporary
-        v-model="left"
-      ></v-navigation-drawer>
-      <v-container fluid>
-        <!--v-router-->
-      </v-container>
-      <v-navigation-drawer
-        right
-        temporary
-        v-model="right"
-      ></v-navigation-drawer>
-    </main>
-    <v-footer class="indigo">
-      <span>© 2017</span>
-    </v-footer>
-  </v-app>
-</template>
-
 <script>
+  import axios from 'axios'
   export default {
     data () {
       return {
         drawer: true,
-        drawerRight: true,
-        right: null,
-        left: null
+        left: null,
+        result: 'Unauthenticated',
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      login () {
+        axios.post(`/api/public/users/${this.username}/session`, {
+          password: this.password
+        }).then((res) => {
+          this.result = res.data.result
+        }).catch(e => {
+          if (e.response) {
+            this.result = 'Login'
+          } else {
+            this.result = 'error'
+          }
+        })
+      },
+      logout () {
+        axios.delete(`/api/private/users/${this.username}`).then((res) => {
+          this.result = res.data.result
+        }).catch(e => {
+          if (e.response) {
+            this.result = e.response.data.result
+          } else {
+            this.result = 'error'
+          }
+        })
       }
     }
   }
