2558b3e0f8207efdaf6371017450ecad522afc49
devori.lee <devori.lee@gmail.com>
2017-07-02 14:14:21 +0900

    change build directory to test of backend

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../../backend/public/index.html'),
-    assetsRoot: path.resolve(__dirname, '../../backend/public'),
+    index: path.resolve(__dirname, '../../backend/app/public/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../backend/app/public'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
