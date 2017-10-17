70fef8ee012ed58dbed766d376eca8944643017d
devori.lee <devori.lee@gmail.com>
2017-10-17 22:41:33 +0900

    add type(buy or sell) to history page. skip BTC coin at history

--- frontend/src/containers/History.vue
+++ frontend/src/containers/History.vue
@@ -19,12 +19,15 @@
         <v-data-table
             :headers="headers"
             :items="listHistories"
+            :pagination.sync="pagination"
             class="elevation-1 text-xs-center"
           >
           <template slot="items" scope="props">
             <td class="text-xs-center">{{ props.item.vcType }}</td>
-            <td class="text-xs-right">{{ props.item.units }}</td>
-            <td class="text-xs-right">{{ props.item.rate }}</td>
+            <td class="text-xs-center">{{ props.item.type }}</td>
+            <td class="text-xs-right">{{ props.item.units.toFixed(8) }}</td>
+            <td v-if="props.item.type !== 'sell'" class="text-xs-right">{{ props.item.rate }}</td>
+            <td v-if="props.item.type === 'sell'" class="text-xs-right">{{ props.item.rate }}({{ props.item.buy }})</td>
             <td class="text-xs-right">{{ (props.item.units * props.item.rate) }}</td>
             <td class="text-xs-center">{{ new Date(props.item.timestamp).toLocaleString() }}</td>
           </template>
@@ -42,7 +45,11 @@
     data () {
       return {
         histories: {},
-        active: ''
+        active: '',
+        pagination: {
+          sortBy: 'timestamp',
+          descending: true
+        }
       }
     },
     filters: {
@@ -54,6 +61,7 @@
       headers () {
         return [
           { text: 'Coin', value: 'vcType' },
+          { text: 'Type', value: 'type' },
           { text: 'Units', value: 'units' },
           { text: 'Rate', value: 'rate' },
           { text: 'Estimated Value', value: 'total' },
@@ -63,6 +71,9 @@
       listHistories () {
         let result = []
         for (let vcType in this.histories) {
+          if (vcType === 'BTC') {
+            continue
+          }
           this.histories[vcType].forEach(h => result.push(h))
         }
         return result
